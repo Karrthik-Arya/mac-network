337f683f5044b0131629d98216b02c5d952ea682
Karrthik Arya <karrthikarya@gmail.com>
2024-08-08 22:58:33 +0530

    Change Image Indexing

--- extract_features.py
+++ extract_features.py
@@ -65,16 +65,18 @@ def run_batch(cur_batch, model):
 
 def main(args):
   input_paths = []
-  idx_set = set()
+  # idx_set = set()
+  max_idx = 0
   for fn in os.listdir(args.input_image_dir):
     print(fn)
     if not fn.endswith('.jpg'): continue
     idx = int(os.path.splitext(fn)[0].split('_')[-1])
     input_paths.append((os.path.join(args.input_image_dir, fn), idx))
-    idx_set.add(idx)
+    max_idx = max(max_idx, idx)
+    # idx_set.add(idx)
   input_paths.sort(key=lambda x: x[1])
-  assert len(idx_set) == len(input_paths)
-  assert min(idx_set) == 0 and max(idx_set) == len(idx_set) - 1
+  # assert len(idx_set) == len(input_paths)
+  # assert min(idx_set) == 0 and max(idx_set) == len(idx_set) - 1
   if args.max_images is not None:
     input_paths = input_paths[:args.max_images]
   print(input_paths[0])
@@ -87,6 +89,7 @@ def main(args):
     feat_dset = None
     i0 = 0
     cur_batch = []
+    idxs = []
     for i, (path, idx) in enumerate(input_paths):
       img = io.imread(path)
       img = transform.resize(img, img_size)
@@ -95,15 +98,19 @@ def main(args):
       if len(cur_batch) == args.batch_size:
         feats = run_batch(cur_batch, model)
         if feat_dset is None:
-          N = len(input_paths)
+          N = max_idx
           _, C, H, W = feats.shape
           feat_dset = f.create_dataset('features', (N, C, H, W),
                                        dtype=np.float32)
         i1 = i0 + len(cur_batch)
-        feat_dset[i0:i1] = feats
+        # feat_dset[i0:i1] = feats
+        for i in range(len(idxs)):
+          feat_dset[idxs[i]] = feats[i]
+          print(f"Added {idxs[i]}")
         i0 = i1
         print('Processed %d / %d images' % (i1, len(input_paths)))
         cur_batch = []
+        idxs = []
     if len(cur_batch) > 0:
       feats = run_batch(cur_batch, model)
       i1 = i0 + len(cur_batch)
